ea1f217bf039e68ef930b5e1da5d0fb80b82a08c
rabiyarumi <rabiyaarumi@gmail.com>
2025-01-26 02:37:16 +0600

    impliment delete fav-biodata

--- src/Pages/FavouritesBiodata/FavouritesBiodata.jsx
+++ src/Pages/FavouritesBiodata/FavouritesBiodata.jsx
@@ -2,12 +2,13 @@ import LoadingSpinner from "@/components/shared/LoadingSpinner";
 import useAuth from "@/hooks/useAuth";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import Swal from "sweetalert2";
 
 const FavouritesBiodata = () => {
   const { user } = useAuth();
 
   // Fetch Favorite biodata
-  const { data: favoriteBiodata = [], isLoading: isBiodataLoading } = useQuery({
+  const { data: favoriteBiodata = [], isLoading: isBiodataLoading , refetch} = useQuery({
     queryKey: ["favoriteBiodata"], // Include id in queryKey to ensure query invalidation
     queryFn: async () => {
       const { data } = await axios(
@@ -17,7 +18,43 @@ const FavouritesBiodata = () => {
     },
   });
 
-  console.log(favoriteBiodata);
+  //----------------delete favorite biodatas-----------------\\
+  const handleDelete = (_id) => {
+    try{
+     Swal.fire({
+       title: "Are you sure?",
+       text: "You won't be able to revert this!",
+       icon: "warning",
+       showCancelButton: true,
+       confirmButtonColor: "#3085d6",
+       cancelButtonColor: "#d33",
+       confirmButtonText: "Yes, delete it!",
+     }).then((result) => {
+       if (result.isConfirmed) {
+         //delete data from database
+   
+         const { data } = axios.delete(
+           `${import.meta.env.VITE_API_URL}/fav-biodatas/${_id}`
+         );
+         console.log(data)
+         Swal.fire({
+           title: "Deleted!",
+           text: "Your Biodata been deleted.",
+           icon: "success",
+         });
+        refetch()
+       }
+     })
+    }
+    catch{
+     Swal.fire({
+       icon: "error",
+       title: "Oops...",
+       text: "Something is wrong!",
+    })}
+    
+     // sweet alart end
+   } 
 
   if (isBiodataLoading) return <LoadingSpinner />;
 
@@ -33,7 +70,9 @@ const FavouritesBiodata = () => {
           <div key={biodata._id} className="flex gap-8">
             <h1>{biodata.favBiodata.name}</h1>
             <h1>{biodata.favBiodata.biodataId}</h1>
-            <button className="text-red-700">Delete</button>
+            <button className="text-red-700">
+              <button onClick={() => handleDelete(biodata._id)}>Delete</button>
+            </button>
           </div>
         ))}
           </div> : 
