8e6d35507065bfab4d91b74dd0185f849e570bf5
rabiyarumi <rabiyaarumi@gmail.com>
2025-01-25 20:14:40 +0600

    showing all biodata

--- src/Pages/FavouritesBiodata/FavouritesBiodata.jsx
+++ src/Pages/FavouritesBiodata/FavouritesBiodata.jsx
@@ -9,20 +9,37 @@ const FavouritesBiodata = () => {
 
 
     // Fetch Favorite biodata 
-  const { data: favBiodata = {}, isLoading: isBiodataLoading } = useQuery({
-    queryKey: ['favBiodata'], // Include id in queryKey to ensure query invalidation
+  const { data: favoriteBiodata = [], isLoading: isBiodataLoading } = useQuery({
+    queryKey: ['favoriteBiodata'], // Include id in queryKey to ensure query invalidation
     queryFn: async () => {
         const { data } = await axios(`${import.meta.env.VITE_API_URL}/fav-biodatas/${user.email}`);
         return data;
     },
 });
 
-console.log(favBiodata)
+console.log(favoriteBiodata)
 
 if (isBiodataLoading ) return <LoadingSpinner/>
+
+
     return (
         <div>
-            Favourate biodata
+            My Favorite biodata
+            <h2>hello</h2>
+
+            <div>
+            {
+                favoriteBiodata?.map(biodata =>
+                 <div key={biodata._id} className='flex gap-8'>
+                    
+                    <h1>{biodata.favBiodata.name}</h1>
+                    <h1>{biodata.favBiodata.biodataId}</h1>
+                    <button className='text-red-700'>Delete</button>
+                   </div>)
+            }
+            </div>
+
+            
         </div>
     );
 };
