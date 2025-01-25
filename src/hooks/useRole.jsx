7f2547dd015b2e903d7948ea2809f388501f641a
rabiyarumi <rabiyaarumi@gmail.com>
2025-01-26 05:34:39 +0600

    make user role hook

---
+++ src/hooks/useRole.jsx
@@ -0,0 +1,26 @@
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "./useAuth";
+import axios from "axios";
+import LoadingSpinner from "@/components/shared/LoadingSpinner";
+
+const useRole = () => {
+    const { user, loading } = useAuth();
+
+    // Fetch Favorite biodata
+ const { data: role, isLoading, refetch } = useQuery({
+   queryKey: ["role", user?.email], 
+   enabled: !loading && !!user?.email,
+   queryFn: async () => {
+     const { data } = await axios(
+       `${import.meta.env.VITE_API_URL}/users/role/${user?.email}`
+     );
+     return data;
+   },
+ });
+
+ if (loading) return <LoadingSpinner />;
+
+    return {role, isLoading, refetch}
+};
+
+export default useRole;
