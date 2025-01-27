4ffec39ab3ebf6a9e91990826b301c2dc0d55251
rabiyarumi <rabiyaarumi@gmail.com>
2025-01-27 06:20:05 +0600

    get my contact request

--- src/Pages/MyContactRequest/MyContactRequest.jsx
+++ src/Pages/MyContactRequest/MyContactRequest.jsx
@@ -2,7 +2,6 @@ import LoadingSpinner from '@/components/shared/LoadingSpinner';
 import useAuth from '@/hooks/useAuth';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
-import React from 'react';
 import Swal from 'sweetalert2';
 
 const MyContactRequest = () => {
@@ -18,6 +17,8 @@ const MyContactRequest = () => {
       return data;
     },
   });
+
+  console.log(contactRequest)
 
   //----------------delete favorite biodatas-----------------\\
 //   const handleDelete = (_id) => {
@@ -61,6 +62,28 @@ const MyContactRequest = () => {
     return (
         <div>
             my contact request
+            <div>
+        {
+          contactRequest.length > 0 ? 
+          <div>
+            {contactRequest?.map((biodata) => (
+          <div key={biodata._id} className="flex gap-8">
+            <h1>{biodata.name}</h1>
+            <h1>{biodata.biodataId}</h1>
+            <h1>{biodata.status}</h1>
+            <button className="text-red-700">
+              {/* <button onClick={() => handleDelete(biodata._id)}>Delete</button> */}
+            </button>
+          </div>
+        ))}
+          </div> : 
+          <div>
+            <p>
+            You  Have No Contact Request
+            </p>
+          </div>
+        }
+      </div>
         </div>
     );
 };
