a88fe9db47fc8399b0dff6236ea4d4a3148050f5
rabiyarumi <rabiyaarumi@gmail.com>
2025-01-27 20:39:50 +0600

    showing all contact request for admin

--- src/Pages/ManageUsers/ManageUsers.jsx
+++ src/Pages/ManageUsers/ManageUsers.jsx
@@ -1,16 +1,17 @@
 import LoadingSpinner from "@/components/shared/LoadingSpinner";
 import { Button } from "@/components/ui/button";
+import useAuth from "@/hooks/useAuth";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 const ManageUsers = () => {
-
+    const {user}= useAuth()
     // Fetch all users
   const { data: allUsers = [], isLoading , refetch} = useQuery({
     queryKey: ["allUsers"], // Include id in queryKey to ensure query invalidation
     queryFn: async () => {
       const { data } = await axios(
-        `${import.meta.env.VITE_API_URL}/users`
+        `${import.meta.env.VITE_API_URL}/users/${user?.email}`
       );
       return data;
     },
@@ -33,10 +34,10 @@ const ManageUsers = () => {
                             <p>Role: {user?.role}</p>
                             <p>status: {user?.status}</p>
                             <Button variant="outline">
-                                <button>Make Admin</button>
+                                <p>Make Admin</p>
                             </Button>
                             <Button variant="outline">
-                                <button>Make Premium</button>
+                                <p>Make Premium</p>
                             </Button>
                         </div>
                     ))
