ad502ff85e8cabf3d5c0ce54db30e832e43f2b19
rabiyarumi <rabiyaarumi@gmail.com>
2025-02-19 19:55:34 +0600

    modify card design

--- src/Pages/AllBiodatas/Biodata.jsx
+++ src/Pages/AllBiodatas/Biodata.jsx
@@ -1,52 +1,56 @@
 /* eslint-disable react/prop-types */
-import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
-import Marquee from 'react-fast-marquee';
-import { Link } from 'react-router-dom';
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Marquee from "react-fast-marquee";
+import { Link } from "react-router-dom";
 
-const Biodata = ({biodata}) => {
-    const {
-        _id,
-        name,
-        image,
-        gender,
-        birth,
-        height,
-        weight,
-        age,
-        occupation,
-        race,
-        fathersName,
-        mothersName,
-        division,
-        district,
-        partner,
-        contact,
-      } = biodata || {}
+const Biodata = ({ biodata }) => {
+  const {
+    _id,
+    name,
+    image,
+    gender,
+    birth,
+    height,
+    weight,
+    age,
+    occupation,
+    race,
+    fathersName,
+    mothersName,
+    division,
+    district,
+    partner,
+    contact,
+  } = biodata || {};
 
-    return (
-        
-            <div>
-        <Link to={`/biodata/${_id}`}>
-           <Card>
-           <img src={image} alt="" className='h-56  w-full rounded-t-xl'/>
-            <CardHeader className="flex flex-row gap-4">
-                
-                <CardTitle className='text-red-600'>{name}</CardTitle>
-            </CardHeader>
-            <CardContent>
-                <p>Age: {age}</p>
-                <p>Race: {race}</p>
-            </CardContent>
-            <CardFooter>
-                <Link to={`/biodata/${_id}`}>
-                <Button >View Details</Button></Link>
-            </CardFooter>
-           </Card>
-        </Link>
-        </div>
-       
-    );
+  return (
+    <div className="h-96">
+      <Link to={`/biodata/${_id}`}>
+        <Card className="flex flex-col" >
+          <img src={image} alt="" className="h-56  w-full rounded-t-xl" />
+          <CardHeader className="flex flex-row gap-4">
+            <CardTitle className="text-red-600">{name}</CardTitle>
+          </CardHeader>
+          <CardContent className="flex-grow" >
+            <p>Age: {age}</p>
+            <p>Race: {race}</p>
+          </CardContent>
+          <CardFooter>
+            <Link to={`/biodata/${_id}`}>
+              <Button>View Details</Button>
+            </Link>
+          </CardFooter>
+        </Card>
+      </Link>
+    </div>
+  );
 };
 
 export default Biodata;
